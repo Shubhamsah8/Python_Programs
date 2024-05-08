2dcfaac1f177f8c517edb242e561a1b629af96ce
Shubhamsah8 <shubhamsah086@gmail.com>
2024-05-08 15:10:44 +0530

    [Shubham] Add: Program to find the Largest of three numbers

---
+++ LargestOfThreeNumbers.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+#Find out the largest number among three
+largest = max(num1, num2, num3)
+print(largest)
