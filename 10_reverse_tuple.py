754d12272e123f852d826c57d911359e13ec5d23
Shubhamsah8 <shubhamsah086@gmail.com>
2024-05-10 10:48:52 +0530

    [Shubham] Add: Program to reverse the tuple

---
+++ 10_reverse_tuple.py
@@ -0,0 +1,17 @@
+"""
+    Program to reverse the tuple
+
+    Parameters:
+        my_tuple (tuple): tuple to get reverse
+
+    Returns:
+        reversed tuple
+"""
+
+
+def reverse_tuple(my_tuple):
+    return my_tuple[::-1]
+
+
+my_tuple = (1, 2, 3, 4)
+print(reverse_tuple(my_tuple))
